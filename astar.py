4bfad31e2eb8aed9274f03153c0abc868561849a
Bryan Eeo <sadboys.gonna.sad@gmail.com>
2019-02-03 23:29:12 +0800

    Add files via upload

---
+++ astar.py
@@ -0,0 +1,30 @@
+"""
+    Artificial Intelligence algorithm that find the shortest possible
+    path from start to end state.
+"""
+from Queue import PriorityQueue
+
+class State(object):
+    def __init__(self, value, parent,
+                    start=0, goal=0, solver=0):
+        self.children = []
+        self.parent = parent
+        self.value = value
+        self.dist = 0
+        if parent:
+            self.path = parent.path[:]
+            self.path.append(value)
+            self.start = parent.start
+            self.goal = parent.goal
+            self.solver = parent.solver
+        else:
+            self.path = [value]
+            self.start = start
+            self.goal = goal
+            self.solver = solver
+
+        def GetDist(self):
+            pass
+
+        def CreateChildren(self):
+            pass
